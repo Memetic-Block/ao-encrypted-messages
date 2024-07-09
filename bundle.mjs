877c26d6ac6726392661a60e5854ecaf87b1ed6d
Jim Toth <jim@artby.city>
2024-07-09 16:44:32 -0400

    Sets up typescript and package info

---
+++ bundle.mjs
@@ -0,0 +1,37 @@
+import { build } from 'esbuild';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
+
+const bundle = async () => {
+  console.log('Building minified web bundle file.');
+  await build({
+    entryPoints: ['./src/web/index.ts'],
+    bundle: true,
+    minify: true,
+    platform: 'browser',
+    target: ['esnext'],
+    format: 'esm',
+    globalName: 'ar.io',
+    plugins: [
+      polyfillNode({
+        polyfills: {
+          crypto: true,
+          process: true,
+          fs: true,
+        },
+      }),
+    ],
+    tsconfig: './tsconfig.web.json',
+    outfile: './bundles/web.bundle.min.js',
+    // bunyan - these are not available in the browser
+    external: ['dtrace-provider', 'fs', 'mv', 'os', 'source-map-support'],
+  })
+    .catch((e) => {
+      console.log(e);
+      process.exit(1);
+    })
+    .then(() => {
+      console.log('Successfully built web bundle.');
+    });
+};
+
+bundle();
